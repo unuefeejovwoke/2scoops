ed2dd283443f6ab44b8df8d5327c9d1993d6f80d
unuefe ejovwoke <vuwill7114@gmail.com>
2023-04-27 23:14:09 +0100

    setup api for the app

--- todo/backend/todo_project/settings.py
+++ todo/backend/todo_project/settings.py
@@ -38,8 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # 3rd party
+    'rest_framework',
+    
     'todos.apps.TodosConfig',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
