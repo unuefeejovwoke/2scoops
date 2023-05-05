2c97819a7a74139c1523495087f5ec446bf7b886
unuefe ejovwoke <vuwill7114@gmail.com>
2023-05-04 20:32:21 -0700

    create model database and made test

--- blogapi/posts/tests.py
+++ blogapi/posts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass'
+        )
+
+        self.post = Post.objects.create(
+            author=self.user,
+            title='Test Post',
+            body='This is a test post.'
+        )
+
+    def test_post_creation(self):
+        post = Post.objects.get(title='Test Post')
+        self.assertEqual(post.author, self.user)
+        self.assertEqual(post.body, 'This is a test post.')
