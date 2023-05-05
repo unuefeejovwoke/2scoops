7c6f4499a4eed27160fc6f5ed862d468f6c55a6b
unuefe ejovwoke <vuwill7114@gmail.com>
2023-05-04 21:57:25 -0700

    connected drf to view, details, update, and delete

--- blogapi/blog_project/settings.py
+++ blogapi/blog_project/settings.py
@@ -38,9 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #rest framework
+    'rest_framework',
+
     #local
     'posts.apps.PostsConfig', #new
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ blogapi/posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import PostList, PostDetail
+
+urlpatterns = [
+    
+    path('', PostList.as_view()),
+    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
+
+]
