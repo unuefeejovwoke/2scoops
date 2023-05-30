a0c3d623fd01222b60072408272be520560ba68a
unuefeejovwoke <vuwill7114@gmail.com>
2023-05-29 20:49:59 -0700

    trying to fix url issues

--- blogapi/blog_project/urls.py
+++ blogapi/blog_project/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import re_path # new import
-
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    re_path(r'^api/v1/rest-auth/', include('rest_auth.urls')), # modified
+    url(r'^api/v1/rest-auth/', include('rest_auth.urls')),  # Use url() instead of path()
 ]
+
+
 
