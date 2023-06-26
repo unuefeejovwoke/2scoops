79f3a91302070dff31c58fed2700e9ef60e77b93
unuefeejovwoke <vuwill7114@gmail.com>
2023-06-25 20:55:32 -0700

    django templates config

--- djangobasic/mysite/views.py
+++ djangobasic/mysite/views.py
@@ -1,4 +1,5 @@
 from django.http  import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("<h3> Hello world </h3>")
+    return render(request, 'home.html')
