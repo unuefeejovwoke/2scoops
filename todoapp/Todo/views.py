9b0183d31e6562779ec60b4a5c770125822edf8f
unuefeejovwoke <vuwill7114@gmail.com>
2023-06-28 22:22:38 -0700

    fetch data and send to template

--- todoapp/Todo/views.py
+++ todoapp/Todo/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from todoapp.models import Todo
+
 def home(request): 
-    return render(request, 'home.html')
+    tasks = Todo.objects.filter(is_completed= False)
+    context = {
+        'tasks':tasks,
+    }
+    return render(request, 'home.html', context)
