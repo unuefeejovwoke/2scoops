fb8c1d7e524ab03306ac8d824ea0d98c10d7cb94
unuefeejovwoke <vuwill7114@gmail.com>
2023-06-28 22:27:55 -0700

    admin list display

--- todoapp/todoapp/admin.py
+++ todoapp/todoapp/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from .models import Todo
 
 # Register your models here.
-admin.site.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('tasks', 'is_completed', 'updated_at')
+    search_fields = ('tasks',)
+admin.site.register(Todo, TodoAdmin)
